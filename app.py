02ac5fc2dfe1e53c9facf783c1e8260f7e39291d
Diptanu Banik <diptanubanik9@gmail.com>
2026-06-29 16:43:28 +0530

    added Procfile and gunicorn

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import mysql.connector
+import os
 
 app = Flask(__name__)
 
@@ -73,4 +74,4 @@ def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
