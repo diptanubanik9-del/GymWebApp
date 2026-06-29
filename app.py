6861090f8d73fed76e44e5c6d5e8793aca4172db
Diptanu Banik <diptanubanik9@gmail.com>
2026-06-29 16:58:37 +0530

    added Procfile and gunicorn

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(host="120-332-575", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
